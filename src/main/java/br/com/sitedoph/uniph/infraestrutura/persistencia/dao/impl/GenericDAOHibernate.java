5589fa55afa485ea10623a7ee8f2d0eb140f2cfd
Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>
2016-02-13 11:21:31 -0200

    ADD: implementações de alguns métodos no GenericDAOHibernate.java

--- src/main/java/br/com/sitedoph/uniph/infraestrutura/persistencia/dao/impl/GenericDAOHibernate.java
+++ src/main/java/br/com/sitedoph/uniph/infraestrutura/persistencia/dao/impl/GenericDAOHibernate.java
@@ -3,42 +3,70 @@ package br.com.sitedoph.uniph.infraestrutura.persistencia.dao.impl;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaQuery;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
 import org.hibernate.criterion.Criterion;
 
 import br.com.sitedoph.uniph.infraestrutura.persistencia.dao.GenericDAO;
 
-public class GenericDAOHibernate
-				<T, ID extends Serializable> 
-					implements GenericDAO<T, ID> {
+public class GenericDAOHibernate<T, ID extends Serializable> implements GenericDAO<T, ID> {
+
+	private final Class<T> CLASSE;
+
+	private final EntityManager ENTITY_MANAGER;
+
+	public GenericDAOHibernate(final EntityManager entityManager, final Class<T> classe) {
+
+		ENTITY_MANAGER = entityManager;
+		CLASSE = classe;
+
+	}
 
 	@Override
 	public T salvarOuAtualizar(T entidade) {
-		// TODO Auto-generated method stub
-		return null;
+		return ENTITY_MANAGER.merge(entidade);
 	}
 
 	@Override
 	public T buscarPorId(ID id) {
-		// TODO Auto-generated method stub
-		return null;
+		return ENTITY_MANAGER.find(CLASSE, id);
 	}
 
 	@Override
 	public List<T> buscarTodos() {
-		// TODO Auto-generated method stub
-		return null;
+
+		final CriteriaQuery<T> query = ENTITY_MANAGER.getCriteriaBuilder().createQuery(CLASSE);
+
+		query.select(query.from(CLASSE));
+
+		return ENTITY_MANAGER.createQuery(query).getResultList();
 	}
 
 	@Override
 	public void excluir(T entidade) {
-		// TODO Auto-generated method stub
-		
+		// ENTITY_MANAGER.merge(entidade);
+		ENTITY_MANAGER.remove(entidade);
 	}
 
 	@Override
-	public List<T> buscarPorCriteria(Criterion... criteria) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<T> buscarPorCriteria(
+			Criterion... criteria) {
+
+		final Session session = 
+				(Session) ENTITY_MANAGER
+				.getDelegate();
+
+		final Criteria crit = 
+				session.createCriteria(CLASSE);
+		
+		for (Criterion criterion : criteria) {
+			crit.add(criterion);
+		}
+		
+		return crit.list();
 	}
 
 	@Override
