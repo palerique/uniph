c2a049173884beae9e0af2c7a06ccd44223898d4
Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>
2016-02-22 17:44:38 -0300

    ADD: CDI

--- src/main/java/br/com/sitedoph/uniph/aplicacao/managedsbeans/UsuarioBean.java
+++ src/main/java/br/com/sitedoph/uniph/aplicacao/managedsbeans/UsuarioBean.java
@@ -1,70 +1,73 @@
 package br.com.sitedoph.uniph.aplicacao.managedsbeans;
 
-import java.util.Collection;
-
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
-import javax.validation.ConstraintViolationException;
-
 import br.com.sitedoph.uniph.aplicacao.util.MensagensUtil;
 import br.com.sitedoph.uniph.dominio.entidades.Usuario;
 import br.com.sitedoph.uniph.dominio.services.UsuarioService;
 
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.validation.ConstraintViolationException;
+import java.util.Collection;
+
 @ViewScoped
-@ManagedBean
+@Named
 public class UsuarioBean {
 
-	private Usuario usuario = new Usuario();
-	private Collection<Usuario> usuarios;
+    private Usuario usuario = new Usuario();
+    private Collection<Usuario> usuarios;
 
-	public void gravar() {
+    @Inject
+    private UsuarioService service;
 
-		try {
-			new UsuarioService().salvarOuAtualizar(usuario);
-			limpar();
-			usuarios = new UsuarioService().buscarTodos();
-			MensagensUtil.info("Usuário foi Cadastrado com Sucesso!");
-		} catch (Exception e) {
+    public void gravar() {
 
-			if (e.getCause() instanceof ConstraintViolationException) {
-				MensagensUtil.adicionarMensagensDeValidacao((ConstraintViolationException) e.getCause());
-			} else if (e.getCause() instanceof javax.persistence.PersistenceException) {
-				MensagensUtil.erro("Login ou endereço de e-mail em uso!");
-			} else {
-				throw e;
-			}
+        try {
+            new UsuarioService().salvarOuAtualizar(usuario);
+            limpar();
+            usuarios = new UsuarioService().buscarTodos();
+            MensagensUtil.info("Usuário foi Cadastrado com Sucesso!");
+        } catch (Exception e) {
 
-		}
+            if (e.getCause() instanceof ConstraintViolationException) {
+                MensagensUtil.adicionarMensagensDeValidacao((ConstraintViolationException) e.getCause());
+            } else if (e.getCause() instanceof javax.persistence.PersistenceException) {
+                MensagensUtil.erro("Login ou endereço de e-mail em uso!");
+            } else {
+                throw e;
+            }
 
-	}
+        }
 
-	public void cancelar() {
-		limpar();
-	}
+    }
 
-	private void limpar() {
-		usuario = new Usuario();
-	}
+    public void cancelar() {
+        limpar();
+    }
 
-	public void remover(Usuario usuario) {
-		new UsuarioService().excluir(usuario);
-		usuarios = new UsuarioService().buscarTodos();
-	}
+    private void limpar() {
+        usuario = new Usuario();
+    }
 
-	public Collection<Usuario> getUsuarios() {
-		if (usuarios == null) {
-			usuarios = new UsuarioService().buscarTodos();
-		}
+    public void remover(Usuario usuario) {
+        new UsuarioService().excluir(usuario);
+        usuarios = new UsuarioService().buscarTodos();
+    }
 
-		return usuarios;
-	}
+    public Collection<Usuario> getUsuarios() {
+        if (usuarios == null) {
+            usuarios = new UsuarioService().buscarTodos();
+        }
 
-	public Usuario getUsuario() {
-		return usuario;
-	}
+        return usuarios;
+    }
 
-	public void setUsuario(Usuario usuario) {
-		this.usuario = usuario;
-	}
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
 
 }
