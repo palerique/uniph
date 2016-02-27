028af09e7633b2b5d9e79c2a656fc092dde602ff
Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>
2016-02-27 17:10:27 -0300

    ADD: login bean inicial

---
+++ src/main/java/br/com/sitedoph/uniph/aplicacao/managedsbeans/LoginBean.java
@@ -0,0 +1,74 @@
+package br.com.sitedoph.uniph.aplicacao.managedsbeans;
+
+import java.io.Serializable;
+
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
+
+import br.com.sitedoph.uniph.dominio.entidades.Usuario;
+import br.com.sitedoph.uniph.dominio.services.UsuarioService;
+
+@Named
+@ViewScoped
+public class LoginBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Usuario usuario = new Usuario();
+
+	@Inject
+	UsuarioService usuarioService;
+
+	private boolean logado;
+
+	public String efetuarLogin() {
+		String outcome = "login";
+
+		Usuario existente = usuarioService.buscarPorLoginESenha(usuario.getLogin(), usuario.getSenha());
+
+		if (existente != null && existente.getId() != null) {
+
+			HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext()
+					.getRequest();
+
+			String uri = request.getRequestURI();
+
+			uri = uri.replace("/uniph/", "");
+			uri = uri.replace(".xhtml", "");
+
+			if (uri.equals("login")) {
+				uri = "usuario";
+			}
+
+			outcome = uri + "?faces-redirect=true";
+
+			setLogado(true);
+
+		} else {
+			usuario = new Usuario();
+			setLogado(false);
+		}
+
+		return outcome;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public boolean isLogado() {
+		return logado;
+	}
+
+	public void setLogado(boolean logado) {
+		this.logado = logado;
+	}
+
+}
