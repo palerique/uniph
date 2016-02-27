da889859c8041f11b79caa00b9d7d05976b56950
Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>
2016-02-27 13:48:56 -0300

    ADD: estrutura inicial de formulário para cadastro de alunos

--- src/main/java/br/com/sitedoph/uniph/aplicacao/managedsbeans/AlunoBean.java
+++ src/main/java/br/com/sitedoph/uniph/aplicacao/managedsbeans/AlunoBean.java
@@ -1,15 +1,46 @@
 package br.com.sitedoph.uniph.aplicacao.managedsbeans;
 
+import java.io.Serializable;
+import java.util.Date;
 
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
-import java.io.Serializable;
+
+import br.com.sitedoph.uniph.dominio.entidades.Aluno;
 
 @Named
 @ViewScoped
 public class AlunoBean implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
+	private Aluno aluno = new Aluno();
+
+	private Date dataUtilNascimento;
+	private Date dataUtilCadastro;
+
+	public Aluno getAluno() {
+		return aluno;
+	}
+
+	public void setAluno(Aluno aluno) {
+		this.aluno = aluno;
+	}
+
+	public Date getDataUtilNascimento() {
+		return dataUtilNascimento;
+	}
+
+	public void setDataUtilNascimento(Date dataUtilNascimento) {
+		this.dataUtilNascimento = dataUtilNascimento;
+	}
+
+	public Date getDataUtilCadastro() {
+		return dataUtilCadastro;
+	}
+
+	public void setDataUtilCadastro(Date dataUtilCadastro) {
+		this.dataUtilCadastro = dataUtilCadastro;
+	}
 
 }
